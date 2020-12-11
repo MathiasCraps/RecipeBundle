2937f582462292389532256e5e52d64d5f2e174b
Mathias Craps <mathias.craps@theoplayer.com>
2020-12-11 23:27:30 +0100

    Convert project to TypeScript

--- recipe-front-end/src/App.js
+++
@@ -1,8 +0,0 @@
-import './App.css';
-import Recipe from './components/Recipe';
-
-function App(props) {
-  return props.recipes.map((data, index) => (<Recipe {...data} key={index}></Recipe>));
-}
-
-export default App;
