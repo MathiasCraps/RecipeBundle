588d77b86ccf63dc10601ec1faae713e73a76fc2
Mathias Craps <>
2020-12-12 22:26:40 +0100

    Split up RecipeList to dedicated component

--- recipe-front-end/src/App.tsx
+++ recipe-front-end/src/App.tsx
@@ -1,8 +1,8 @@
-import { Box, Center, Heading, SimpleGrid } from "@chakra-ui/react";
+import { Box, Center, Heading } from "@chakra-ui/react";
 import React from 'react';
 import { connect } from 'react-redux';
 import './App.css';
-import RecipePreview from './components/RecipePreview';
+import { RecipeList } from "./components/RecipeList";
 import { Recipe } from "./interfaces/Recipe";
 import { ReduxModel, ViewType } from './redux/Store';
 
@@ -19,21 +19,14 @@ function mapStateToProps(props: ReduxModel) {
 }
 
 function App(props: AppProps) {
-  return (
-    <Box>
-      <header>
-        <Center bgColor="gray.100" p="0.5em">
-          <Heading as="h1">Rebundle</Heading>
-        </Center>
-      </header>
-      <SimpleGrid paddingTop="2em"
-        margin="auto"
-        maxWidth="60em"
-        columns={[1, 2, 3, 4]}
-        spacing="1em">
-        {props.recipes.map((data, index) => (<RecipePreview {...data} key={index}></RecipePreview>))}
-      </SimpleGrid>
-    </Box>);
+  return (<Box>
+    <header>
+      <Center bgColor="gray.100" p="0.5em">
+        <Heading as="h1">Rebundle</Heading>
+      </Center>
+    </header>
+    <RecipeList recipes={props.recipes}></RecipeList>
+  </Box>);
 }
 
 export default connect(mapStateToProps, {})(App);
---
+++ recipe-front-end/src/components/RecipeList.tsx
@@ -0,0 +1,18 @@
+import { SimpleGrid } from "@chakra-ui/react";
+import React from "react";
+import { Recipe } from "../interfaces/Recipe";
+import RecipePreview from "./RecipePreview";
+
+interface Props {
+    recipes: Recipe[];
+}
+
+export function RecipeList(props: Props) {
+    return (<SimpleGrid paddingTop="2em"
+    margin="auto"
+    maxWidth="60em"
+    columns={[1, 2, 3, 4]}
+    spacing="1em">
+    {props.recipes.map((data, index) => (<RecipePreview {...data} key={index}></RecipePreview>))}
+  </SimpleGrid>);
+}
