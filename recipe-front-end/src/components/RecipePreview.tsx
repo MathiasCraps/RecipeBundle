075ee63037f5291f8cfc06194d203636b221010e
Mathias Craps <>
2020-12-12 14:29:38 +0100

    Improve styling

--- recipe-front-end/src/components/RecipePreview.tsx
+++ recipe-front-end/src/components/RecipePreview.tsx
@@ -1,4 +1,4 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Box, Image, Heading } from "@chakra-ui/react";
 
 interface RecipePreviewProps {
   title: string;
@@ -7,10 +7,14 @@ interface RecipePreviewProps {
 }
 
 function RecipePreview(props: RecipePreviewProps) {
-  return (<Box borderWidth="1px" cursor="pointer">
-      <h2>{props.title}</h2>
-      <Image src={props.image} maxWidth="100%" alt="" />
-      <div>{props.content}</div>
+  return (<Box
+    borderWidth="1px"
+    cursor="pointer"
+    p="0.5em"
+    borderRadius="lg">
+    <Heading as="h2">{props.title}</Heading>
+    <Image src={props.image} maxWidth="100%" alt="" />
+    <div>{props.content}</div>
   </Box>);
 }
 
