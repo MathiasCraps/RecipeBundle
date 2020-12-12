47bf709c89724b42bebd0912d19e04d2d6513bb4
Mathias Craps <>
2020-12-12 14:18:40 +0100

    Introduce basic responsive grid

--- recipe-front-end/src/App.tsx
+++ recipe-front-end/src/App.tsx
@@ -1,13 +1,21 @@
+import React from 'react';
 import './App.css';
 import RecipePreview from './components/RecipePreview';
 import { Recipe } from './interfaces/Recipe';
+import { SimpleGrid } from "@chakra-ui/react";
 
 interface AppProps {
     recipes: Recipe[];
 }
 
 function App(props: AppProps) {
-  return <div>{props.recipes.map((data, index) => (<RecipePreview {...data} key={index}></RecipePreview>))}</div>;
+  return (<SimpleGrid
+    margin="auto"
+    maxWidth="60em"
+    columns={[1, 2, 3, 4]}
+    spacing="1em">
+    {props.recipes.map((data, index) => (<RecipePreview {...data} key={index}></RecipePreview>))}
+  </SimpleGrid>);
 }
 
 export default App;
--- recipe-front-end/src/components/RecipePreview.tsx
+++ recipe-front-end/src/components/RecipePreview.tsx
@@ -1,13 +1,17 @@
-interface RecipePreviewProps  {
+import { Box, Image } from "@chakra-ui/react";
+
+interface RecipePreviewProps {
   title: string;
   content: string;
+  image: string;
 }
 
 function RecipePreview(props: RecipePreviewProps) {
-  return (<article>
+  return (<Box borderWidth="1px" cursor="pointer">
       <h2>{props.title}</h2>
-      <div>{props.content}</div>                             
-    </article>);
+      <Image src={props.image} maxWidth="100%" alt="" />
+      <div>{props.content}</div>
+  </Box>);
 }
 
 export default RecipePreview;
