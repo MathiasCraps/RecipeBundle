db6fd112c8f6fde5a3055cefbb8e9811b5d8311e
Mathias Craps <>
2020-12-14 21:59:58 +0100

    Add array icons using chakra icons

--- recipe-front-end/src/components/RecipeOverview.tsx
+++ recipe-front-end/src/components/RecipeOverview.tsx
@@ -1,4 +1,5 @@
 import { Box, Heading, Image, SlideFade, CloseButton } from "@chakra-ui/react";
+import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons"
 import React from "react";
 import { connect } from "react-redux";
 import { Recipe } from "../interfaces/Recipe";
@@ -24,6 +25,8 @@ function mapStateToProps(state: ReduxModel) {
 function RecipeOverview(props: Props) {
     return (<SlideFade in={true}>
         <Box padding="2em" maxWidth="80em" margin="auto">
+            <a href="#"><ArrowBackIcon aria-label="Naar vorig recept" /></a>
+            <a href="#"><ArrowForwardIcon aria-label="Naar volgend recept" /></a>
             <CloseButton className="close-button-recipe-overview" autoFocus={true} size="sm" onClick={() => props.changeActiveView(ViewType.Overview, undefined)} />
             <Heading as="h2">{props.recipe.title}</Heading>
             <Image src={props.recipe.image} alt="" />
