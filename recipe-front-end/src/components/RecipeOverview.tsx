fad155970806e1bcc1c1df7df15762f7f86eefce
Mathias Craps <>
2020-12-15 23:26:24 +0100

    Add listener to recipe overview to allow navigation left and right + allow escaping using ESC key

--- recipe-front-end/src/components/RecipeOverview.tsx
+++ recipe-front-end/src/components/RecipeOverview.tsx
@@ -1,11 +1,11 @@
-import { Box, Heading, Image, SlideFade, CloseButton } from "@chakra-ui/react";
-import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons"
-import React from "react";
+import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
+import { Box, CloseButton, Heading, Image, SlideFade } from "@chakra-ui/react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Recipe } from "../interfaces/Recipe";
+import { Localisation } from "../localisation/AppTexts";
 import { changeActiveView, Direction, switchActiveRecipe } from "../redux/Actions";
 import { ReduxModel, ViewType } from "../redux/Store";
-import { Localisation } from "../localisation/AppTexts";
 
 interface OriginalProps {
     recipe: Recipe;
@@ -24,14 +24,44 @@ function mapStateToProps(state: ReduxModel) {
     };
 }
 
+
+
 function RecipeOverview(props: Props) {
+    useEffect(() => {
+        function handleKeyPress(keyEvent: KeyboardEvent) {
+            let direction: Direction | undefined;
+
+            if (keyEvent.code === 'Escape') {
+                props.changeActiveView(ViewType.Overview, undefined);
+                return;
+            }
+            
+            switch (keyEvent.code) {
+                case "ArrowLeft":
+                    direction = Direction.PREVIOUS;
+                    break;
+                case "ArrowRight":
+                    direction = Direction.NEXT;
+                    break;
+            }
+            if (direction !== undefined) {
+                props.switchActiveRecipe(props.recipe, direction)
+            }
+
+        }
+
+        document.body.addEventListener('keyup', handleKeyPress);
+        return () => {
+            document.body.removeEventListener('keyup', handleKeyPress);
+        }
+    })
     return (<SlideFade in={true}>
         <Box padding="2em" maxWidth="80em" margin="auto">
             <a href="#" onClick={() => props.switchActiveRecipe(props.recipe, Direction.PREVIOUS)} >
-                <ArrowBackIcon boxSize="2em" aria-label={Localisation.PREVIOUS_RECIPE}/>
+                <ArrowBackIcon boxSize="2em" aria-label={Localisation.PREVIOUS_RECIPE} />
             </a>
             <a href="#" onClick={() => props.switchActiveRecipe(props.recipe, Direction.NEXT)}>
-                <ArrowForwardIcon boxSize="2em" aria-label={Localisation.NEXT_RECIPE}/>
+                <ArrowForwardIcon boxSize="2em" aria-label={Localisation.NEXT_RECIPE} />
             </a>
             <CloseButton className="close-button-recipe-overview" autoFocus={true} size="sm" onClick={() => props.changeActiveView(ViewType.Overview, undefined)} />
             <Heading as="h2">{props.recipe.title}</Heading>
