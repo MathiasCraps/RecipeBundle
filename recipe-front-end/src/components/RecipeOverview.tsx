bca76a2bc21df87b2973c8e265228eb49c568186
Mathias Craps <>
2020-12-15 22:18:41 +0100

    Centralize text shown in the application to a central enum

--- recipe-front-end/src/components/RecipeOverview.tsx
+++ recipe-front-end/src/components/RecipeOverview.tsx
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import { Recipe } from "../interfaces/Recipe";
 import { changeActiveView, Direction, switchActiveRecipe } from "../redux/Actions";
 import { ReduxModel, ViewType } from "../redux/Store";
+import { Localisation } from "../localisation/AppTexts";
 
 interface OriginalProps {
     recipe: Recipe;
@@ -27,17 +28,17 @@ function RecipeOverview(props: Props) {
     return (<SlideFade in={true}>
         <Box padding="2em" maxWidth="80em" margin="auto">
             <a href="#" onClick={() => props.switchActiveRecipe(props.recipe, Direction.PREVIOUS)} >
-                <ArrowBackIcon aria-label="Naar vorig recept" />
+                <ArrowBackIcon boxSize="2em" aria-label={Localisation.PREVIOUS_RECIPE}/>
             </a>
             <a href="#" onClick={() => props.switchActiveRecipe(props.recipe, Direction.NEXT)}>
-                <ArrowForwardIcon aria-label="Naar volgend recept" />
+                <ArrowForwardIcon boxSize="2em" aria-label={Localisation.NEXT_RECIPE}/>
             </a>
             <CloseButton className="close-button-recipe-overview" autoFocus={true} size="sm" onClick={() => props.changeActiveView(ViewType.Overview, undefined)} />
             <Heading as="h2">{props.recipe.title}</Heading>
             <Image src={props.recipe.image} alt="" />
-            <Heading as="h3">Ingrediënten</Heading>
+            <Heading as="h3">{Localisation.INGREDIENTS}</Heading>
             {props.recipe.steps.map((step, index) => <p key={index}>{step}</p>)}
-            <Heading as="h3">Instructies</Heading>
+            <Heading as="h3">{Localisation.STEPS}</Heading>
             {props.recipe.steps.map((step, index) => <p key={index}>{step}</p>)}
         </Box>
     </SlideFade>);
