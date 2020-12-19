b34bf35a44da2535ba5e03d7029c369776fde36c
Mathias Craps <mathias.craps@gmail.com>
2020-12-19 18:10:24 +0100

    Set up simple database connection foundation using environment variables

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const fs = require('fs')
 const cors = require('cors');
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
 
 const app = express();
 const port = 8080;
@@ -15,4 +17,18 @@ app.get('/getRecipes', async (error, response) => {
 
 app.listen(port, () => {
     console.log(`Server active at http://localhost:${port}`)
+});
+
+dotenv.config();
+
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT
+});
+
+pool.connect((error, client, done) => {
+    console.log('success');
 });
