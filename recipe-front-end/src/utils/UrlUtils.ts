95dd4c06a6b0eb471362dd9104915d59d28c9487
Mathias Craps <mathias.craps@gmail.com>
2020-12-22 23:56:51 +0100

    Fix bug where key/values would include equal sign

--- recipe-front-end/src/utils/UrlUtils.ts
+++ recipe-front-end/src/utils/UrlUtils.ts
@@ -8,7 +8,7 @@ export function parseGetParams(getParamsString: string): GetParamsObject[] {
         var subIndex = entry.indexOf('=');
         var hasValue = subIndex > -1;
         var key = hasValue ? entry.substring(0, subIndex) : entry;
-        var value = hasValue ? entry.substring(subIndex, entry.length) : undefined;
+        var value = hasValue ? entry.substring(subIndex + 1, entry.length) : undefined;
         return { key, value };
     });
 }
