b7624385381f1648603ee4b8ab064a2f90d4eec9
Mathias Craps <mathias.craps@gmail.com>
2020-12-25 09:35:39 +0100

    Improve resuls for parseGetParams function so an object instead of an array is used (better usability)

--- recipe-front-end/src/utils/UrlUtils.ts
+++ recipe-front-end/src/utils/UrlUtils.ts
@@ -1,15 +1,18 @@
-export interface GetParamsObject {
-    key: string;
-    value: string | undefined;
+export interface GetParams {
+    [key: string]: string | undefined;
 }
 
-export function parseGetParams(getParamsString: string): GetParamsObject[] {
-    return getParamsString.replace('?', '').split('&').map((entry) => {
+export function parseGetParams(getParamsString: string): GetParams {
+    const queryMap: GetParams = {};
+    getParamsString.replace('?', '').split('&').forEach((entry) => {
         const subIndex = entry.indexOf('=');
         const hasValue = subIndex > -1;
         const key = hasValue ? entry.substring(0, subIndex) : entry;
         const value = hasValue ? entry.substring(subIndex + 1, entry.length) : undefined;
-        return { key, value };
+
+        queryMap[key] = value
     });
+
+    return queryMap;
 }
 
