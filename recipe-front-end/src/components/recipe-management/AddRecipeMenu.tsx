ff4006add0e643cdd1e7d622eae6b0a17a766911
Mathias Craps <mathias.craps@gmail.com>
2021-01-10 18:43:34 +0100

    Adjust interface for creating ingredients so quantities can be added via a modal

--- recipe-front-end/src/components/recipe-management/AddRecipeMenu.tsx
+++ recipe-front-end/src/components/recipe-management/AddRecipeMenu.tsx
@@ -1,14 +1,19 @@
-import { Box, Button, CloseButton, Heading, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, SlideFade, Textarea, useToast } from "@chakra-ui/react";
-import React, { ChangeEvent, useRef, useState } from "react";
+import { Box, Button, CloseButton, Heading, Input, SlideFade, Textarea, useToast } from "@chakra-ui/react";
+import { faPencilAlt, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useRef, useState } from "react";
 import { connect } from "react-redux";
 import { AddRecipeResponse } from "../../interfaces/AddRecipeResponse";
 import { Recipe } from "../../interfaces/Recipe";
 import { Localisation } from "../../localisation/AppTexts";
 import { changeActiveView, updateRecipes } from "../../redux/Actions";
-import { OpenedMenu, ReduxModel, ViewType } from "../../redux/Store";
+import { ReduxModel, ViewType } from "../../redux/Store";
+import { IngredientsModal } from "./IngredientsModal";
 
-interface IngredientInput {
-    value: string;
+export interface IngredientInput {
+    name: string;
+    quantityNumber: number;
+    quantityDescription: string;
     identifier: number;
 }
 
@@ -32,33 +37,20 @@ function mapStateToProps(reduxModel: ReduxModel): ComponentProps {
     }
 }
 
+function createEmptyIngredient() {
+    return {
+        name: '',
+        identifier: ++index,
+        quantityDescription: '',
+        quantityNumber: 0
+    }
+}
+
 export function AddRecipeMenu(props: Props) {
     const toast = useToast();
 
     function close() {
         props.changeActiveView(ViewType.Overview, undefined);
-    }
-
-    function updateIngredientsInputs(event: ChangeEvent<HTMLInputElement>, index: number) {
-        const copyOfIngredients = [...ingredients]; // always make a copy for immutability
-
-        copyOfIngredients[index].value = event.target.value; // update the entry
-
-        const entryCount0Based = copyOfIngredients.length - 1;
-        const lastEntry = copyOfIngredients[entryCount0Based];
-        if (lastEntry.value) {
-            copyOfIngredients[entryCount0Based + 1] = {
-                value: '',
-                identifier: ++index
-            }
-        }
-
-        const emptiedFilter = copyOfIngredients.filter((entry, index) => {
-            const lastEntry = (index + 1) === copyOfIngredients.length;
-            return entry.value || lastEntry;
-        });
-
-        setIngredients(emptiedFilter);
     }
 
     async function postRecipe() {
@@ -67,8 +59,11 @@ export function AddRecipeMenu(props: Props) {
         }
 
         const transformedIngredients = ingredients.slice(0, ingredients.length - 1).map((ingredient) => {
-            return { quantity: '1 stuk', name: ingredient.value } // todo: remove me by adding quantity option to interface!
-        })
+            return {
+                name: ingredient.name,
+                quantity: `${ingredient.quantityNumber} ${ingredient.quantityDescription}`
+            };
+        });
         const recipeData: Recipe = {
             title,
             ingredients: transformedIngredients,
@@ -111,20 +106,22 @@ export function AddRecipeMenu(props: Props) {
             })
         }
 
-        setIngredients([{ value: '', identifier: ++index }]);
+        setIngredients([]);
         setTitle('');
         setSteps('');
         setImagePath('');
+        setEditingIngredient(undefined);
         close();
     }
 
-    const [ingredients, setIngredients] = useState([{ value: '', identifier: ++index }]);
+    const [ingredients, setIngredients] = useState<IngredientInput[]>([]);
     const [title, setTitle] = useState('');
     const [steps, setSteps] = useState('');
     const [imagePath, setImagePath] = useState('');
+    const [editingIngredient, setEditingIngredient] = useState<IngredientInput>();
     const ref = useRef<HTMLInputElement>(null)
 
-    const canBeSubmitted = Boolean(ingredients.length > 1 && title && steps && imagePath);
+    const canBeSubmitted = Boolean(ingredients.length && title && steps && imagePath);
 
     return (<Box>
         <CloseButton className="close-button-top-left" autoFocus={true} size="md" onClick={() => props.changeActiveView(ViewType.Overview, undefined)} />
@@ -134,8 +131,39 @@ export function AddRecipeMenu(props: Props) {
             <br /><br />
             <h2>{Localisation.INGREDIENTS}</h2>
             {ingredients.map((ingredient: IngredientInput, index: number) => {
-                return <Input placeholder={Localisation.INGREDIENT} key={index} value={ingredient.value} onChange={(event) => updateIngredientsInputs(event, index)} />
+                const {name, quantityNumber, quantityDescription} = ingredient;
+                return (<Box className="edit-ingredient-container" key={index}>
+                    <label>
+                        <Button onClick={() => setEditingIngredient(ingredient)}><FontAwesomeIcon icon={faPencilAlt}/></Button>
+                        <strong>{name}</strong>, {quantityNumber} {quantityDescription}
+                    </label>
+                </Box>)
             })}
+
+            
+            {editingIngredient && <IngredientsModal 
+                onConfirm={(ingredientInput: IngredientInput) => {
+                    const identifier = ingredientInput.identifier;
+                    const shallowCopy = [...ingredients];
+                    const entryToReplace = shallowCopy.filter((ingredient) => ingredient.identifier === identifier);
+                    const indexOfEntryToReplace = shallowCopy.indexOf(entryToReplace[0]);
+
+                    if (indexOfEntryToReplace !== -1) {
+                        shallowCopy[indexOfEntryToReplace] = ingredientInput;
+                    } else {
+                        shallowCopy.push(ingredientInput);
+                    }
+
+                    setEditingIngredient(undefined);
+                    setIngredients(shallowCopy);
+                }}
+                onCancel={() => {
+                    setEditingIngredient(undefined)
+                }}
+            ingredientInputs={editingIngredient} />}
+            
+
+            <Button onClick={() =>setEditingIngredient(createEmptyIngredient())}><FontAwesomeIcon icon={faPlus}/></Button>
             <br /><br /><br />
             <Textarea placeholder={Localisation.STEP} onChange={(event) => setSteps(event.target.value)} />
             <input ref={ref} type="file" accept="image/jpeg, image/png" onChange={(event) => setImagePath(event.target.value)} />
---
+++ recipe-front-end/src/components/recipe-management/IngredientsModal.tsx
@@ -0,0 +1,54 @@
+import { Button, Heading, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalOverlay } from "@chakra-ui/react";
+import React, { useRef, useState } from "react";
+import { Localisation } from "../../localisation/AppTexts";
+import { IngredientInput } from "./AddRecipeMenu";
+
+interface OwnProps {
+    ingredientInputs: IngredientInput;
+    onConfirm: (newValue: IngredientInput) => void;
+    onCancel: () => void;
+}
+
+const quantityDescriptions = ['stuk', 'gram', 'eetlepel', 'theelepel', 'snufje'];
+
+export function IngredientsModal(props: OwnProps) {
+    const focusRef = useRef<HTMLInputElement>(null);
+    const [name, setName] = useState(props.ingredientInputs.name);
+    const [quantityNumber, setQuantityNumber] = useState(props.ingredientInputs.quantityNumber);
+    const [quantityDescription, setQuantityDescription] = useState<string>(props.ingredientInputs.quantityDescription || quantityDescriptions[0]);
+    const canBeSubmitted = name && quantityNumber;
+
+    return (<Modal isOpen={true} onClose={props.onCancel} initialFocusRef={focusRef}>
+        <ModalOverlay />
+        <ModalContent>
+            <ModalCloseButton onClick={props.onCancel} />
+            <ModalBody>
+                <Heading as="h3">{Localisation.EDIT_INGREDIENT}</Heading>
+                <Input  ref={focusRef} 
+                        value={name} 
+                        onChange={(e) => setName(e.target.value)} 
+                        placeholder={Localisation.INGREDIENT_NAME} 
+                />
+                <Input  value={quantityNumber || ''} 
+                        onChange={(e) => setQuantityNumber(Number(e.target.value) || 0)} 
+                        type='number'
+                        placeholder={Localisation.QUANTITY} 
+                />
+                <select onChange={(e) => setQuantityDescription(e.target.selectedOptions[0].value)}>
+                    {quantityDescriptions.map((description, index) => {
+                        return <option selected={description === props.ingredientInputs.quantityDescription} key={index} value={description}>{description}</option>
+                    })}
+                </select>
+            </ModalBody>
+            <ModalFooter>
+                <Button colorScheme="blue" disabled={!canBeSubmitted} onClick={(e) => props.onConfirm({
+                    name,
+                    quantityNumber,
+                    quantityDescription,
+                    identifier: props.ingredientInputs.identifier
+                })}>{Localisation.ADD}</Button>
+                <Button variant="ghost" onClick={(e) => props.onCancel()}>{Localisation.CANCEL}</Button>
+            </ModalFooter>
+        </ModalContent>
+    </Modal>);
+}
