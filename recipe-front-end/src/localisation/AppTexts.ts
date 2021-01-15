ac0bbca81eebde5fdd66a3751e1f048346c3e9ad
Mathias Craps <mathias.craps@gmail.com>
2021-01-15 23:20:52 +0100

    Add MenuPlanner as string value

--- recipe-front-end/src/localisation/AppTexts.ts
+++ recipe-front-end/src/localisation/AppTexts.ts
@@ -24,5 +24,6 @@ export enum Localisation {
     CANCEL = 'Annuleren',
     TITLE = 'Titel',
     ADDING_WAS_SUCCESS = 'Gelukt! Bedankt voor je inzending.',
-    ADDING_FAILED = 'Jammer! We konden je gerecht niet toevoegen. Probeer later opnieuw.'
+    ADDING_FAILED = 'Jammer! We konden je gerecht niet toevoegen. Probeer later opnieuw.',
+    MENU_PLANNER = 'Menuplanner'
 }
