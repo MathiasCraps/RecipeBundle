65b8fab6b59c761130ed6c81986ce55faeab19ec
Mathias Craps <mathias.craps@gmail.com>
2021-01-21 23:48:36 +0100

    Add TypeGuard for RecipeData check on back-end

--- back-end/src/validation/TypeGuards.ts
+++ back-end/src/validation/TypeGuards.ts
@@ -1,4 +1,4 @@
-import { Recipe } from "../model/RecipeData";
+import { DayMenu, Recipe } from "../model/RecipeData";
 
 export function isRecipe(data: unknown): data is Recipe {
     const recipe = data as Recipe;
@@ -7,4 +7,13 @@ export function isRecipe(data: unknown): data is Recipe {
     }
 
     return Boolean(recipe.title && recipe.ingredients?.length && recipe.steps);
+}
+
+export function isDayMenu(data: unknown): data is DayMenu {
+    const dayMenu = data as DayMenu;
+    if (typeof dayMenu !== 'object' || dayMenu === null) {
+        return false;
+    }
+
+    return Boolean(typeof dayMenu.date === 'number' && isRecipe(dayMenu.recipe));
 }
