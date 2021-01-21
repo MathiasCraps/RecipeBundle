0d40093af94a1b49b937da40948e380a28f52d77
Mathias Craps <mathias.craps@gmail.com>
2021-01-21 22:07:41 +0100

    Remove unused redux actions for Day component

--- recipe-front-end/src/components/menu-planner/Day.tsx
+++ recipe-front-end/src/components/menu-planner/Day.tsx
@@ -1,29 +1,14 @@
 import { Center, useMediaQuery } from "@chakra-ui/react";
 import React from "react";
 import { connect } from "react-redux";
-import { Dispatch } from "redux";
 import { Localisation } from "../../localisation/AppTexts";
-import { addMenu } from "../../redux/Actions";
-import { AddMenuAction, DayMenu } from "../../redux/Store";
 
-interface OwnProps {
+interface Props {
     date: Date;
     isCurrentDay: boolean;
     amountOfRecipes: number;
     onFocus(): void;
 }
-
-interface ReduxActionProps {
-    addMenu: (menu: DayMenu) => Promise<void>;
-}
-
-function mapDispatchToProps(dispatch: Dispatch<AddMenuAction>): ReduxActionProps {
-    return {
-        addMenu: addMenu(dispatch)
-    }
-}
-
-type Props = OwnProps & ReduxActionProps;
 
 function Day(props: Props) {
     const classes = `day ${props.isCurrentDay ? 'selected-day' : ''}`;
@@ -43,4 +28,4 @@ function Day(props: Props) {
     </div>);
 }
 
-export default connect(null, mapDispatchToProps)(Day);
+export default connect(null, null)(Day);
