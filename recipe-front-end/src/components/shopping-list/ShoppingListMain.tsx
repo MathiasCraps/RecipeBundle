f0bdf8feb1f4d1da94252298c0254be2bb9c0d03
Mathias Craps <mathias.craps@gmail.com>
2021-02-09 00:19:23 +0100

    Pass RulesHandler as argument for improved testability

--- recipe-front-end/src/components/shopping-list/ShoppingListMain.tsx
+++ recipe-front-end/src/components/shopping-list/ShoppingListMain.tsx
@@ -29,6 +29,11 @@ function formatDate(date: Date) {
     return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
 }
 
+const rulesHandler = new RulesHandler([
+    new TableSpoonToGramRule(), 
+    new TeaSpoonToGramRule()
+]);
+
 export function ShoppingListMain(props: ReduxProps) {
     const startTime = calculateStartOfDate(new Date());
     const endTime = new Date(startTime.getTime() + (FULL_DAY_IN_MS * 7));
@@ -36,7 +41,7 @@ export function ShoppingListMain(props: ReduxProps) {
     const menusToConsider = selectMenuFromRange(props.menus, startTime, endTime);
     const ingredientsFromRecipes = menusToConsider.map(e => e.recipe.ingredients).flat(1);
     const rawSorted = sortByIngredient(ingredientsFromRecipes);
-    const sumsToRender = combineToSingleValue(rawSorted);
+    const sumsToRender = combineToSingleValue(rawSorted, rulesHandler);
 
     return <ContentContainer classes="shopping-list">
         <h2>{Localisation.SHOPPING_LIST}</h2>
--- recipe-front-end/src/components/shopping-list/normalization/Combiner.ts
+++ recipe-front-end/src/components/shopping-list/normalization/Combiner.ts
@@ -1,15 +1,8 @@
 import { Ingredient } from '../../../interfaces/Recipe';
-import { TableSpoonToGramRule } from './rules/TableSpoonToGramRule';
-import { TeaSpoonToGramRule } from './rules/TeaSpoonToGramRule';
 import { RulesHandler } from './RulesHandler';
 import { SortedRecipeMap } from './SortRecipeMap';
 
-const rulesHandler = new RulesHandler([
-    new TableSpoonToGramRule(), 
-    new TeaSpoonToGramRule()
-]);
-
-export function combineToSingleValue(sortedRecipeMap: SortedRecipeMap): Ingredient[] {
+export function combineToSingleValue(sortedRecipeMap: SortedRecipeMap, rulesHandler: RulesHandler): Ingredient[] {
     const keys = Object.keys(sortedRecipeMap);
     return keys.map((key) => {
         const recipeMap: Ingredient[] = sortedRecipeMap[key];
