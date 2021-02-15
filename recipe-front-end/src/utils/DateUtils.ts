42963940887cdcaee79a0e6ceaf35f3163709dc2
Mathias Craps <mathias.craps@gmail.com>
2021-02-15 21:41:11 +0100

    Add utlity to calculate month grid

--- recipe-front-end/src/utils/DateUtils.ts
+++ recipe-front-end/src/utils/DateUtils.ts
@@ -10,3 +10,22 @@ export function normalizeWeekDay(day: number): number {
     }
     return day - 1;
 }
+
+export function calculateMonthGrid(year: number, month: number): Date[][] {
+    const amountOfDays = new Date(year, month+1, 0).getDate();
+    const calendar: Date[][] = [];
+    let week = -1;
+    let startOfCurrentWeek = 1;
+
+    for (let i = 1; i <= amountOfDays; i++) {
+        const proposedDate = new Date(Date.UTC(year, month, i));
+        const normalizedDay = normalizeWeekDay(proposedDate.getDay())
+        if (startOfCurrentWeek === i || normalizedDay === 0) {
+            calendar.push([]);
+            week++;
+            startOfCurrentWeek = proposedDate.getDate();
+        }
+        calendar[week].push(proposedDate);
+    }
+    return calendar;
+}
