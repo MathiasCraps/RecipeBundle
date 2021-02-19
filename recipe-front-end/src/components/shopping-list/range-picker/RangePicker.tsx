9bc1db90853ddfa06dd3b1de0308118579b7eae5
Mathias Craps <mathias.craps@gmail.com>
2021-02-19 21:37:27 +0100

    Rework RangePicker so start and end range can be picked

--- recipe-front-end/src/components/shopping-list/range-picker/RangePicker.tsx
+++ recipe-front-end/src/components/shopping-list/range-picker/RangePicker.tsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Localisation } from '../../../localisation/AppTexts';
+import { updateShoppingRange } from '../../../redux/Actions';
 import { ReduxModel } from '../../../redux/Store';
 import { CalendarMonth } from './CalendarMonth';
 
 interface ReduxProps {
     startDate: Date;
     endDate: Date;
+}
+
+interface ReduxActions {
+    updateShoppingRange: typeof updateShoppingRange;
 }
 
 function formatDate(date: Date) {
@@ -20,7 +25,25 @@ function mapStateToProps(reduxModel: ReduxModel): ReduxProps {
     }
 }
 
-function RangePicker(props: ReduxProps) {
+type Props = ReduxProps & ReduxActions;
+
+function RangePicker(props: Props) {
+    function updateRange(date: Date) {
+        if(isNaN(+date)) {
+            return;
+        }
+
+        if (!selection) {
+            setSelection(date);
+        } else {
+            const [start, end] = [selection, date].sort((a, b) => Number(a) - Number(b));
+            setSelection(undefined);
+            setIsvisible(false);
+            props.updateShoppingRange({start, end});
+        }
+    }
+
+    const [selection, setSelection] = useState<Date>();
     const [isVisible, setIsvisible] = useState(false);
     return <>
         <p>{Localisation.YOUR_SHOPPING_LIST_FOR_THE_PERIOD} <a
@@ -31,9 +54,9 @@ function RangePicker(props: ReduxProps) {
         </a>:</p>
         <CalendarMonth isVisible={isVisible}
             date={new Date()}
-            onDayPicked={(day: Date) => console.log('clicked', day)}
+            onDayPicked={updateRange}
         />
     </>
 }
 
-export default connect(mapStateToProps)(RangePicker);
+export default connect(mapStateToProps, { updateShoppingRange })(RangePicker);
