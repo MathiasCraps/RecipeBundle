41eee748e43de5df47e4d521a47ab95fda548fcc
Mathias Craps <mathias.craps@gmail.com>
2021-02-20 20:38:02 +0100

    Tweak dateIsinRange so UTC-differences can be detected (if same days still)

--- recipe-front-end/src/utils/DateUtils.ts
+++ recipe-front-end/src/utils/DateUtils.ts
@@ -64,5 +64,8 @@ export function addDays(date: Date, days: number): Date {
 }
 
 export function dateIsInRange(dateToCompare: Date, fromDate: Date, toDate: Date): boolean {
+    if (isSameUtcDay(dateToCompare, fromDate) || isSameUtcDay(dateToCompare, toDate)) {
+        return true;
+    }
     return fromDate <= dateToCompare && toDate >= dateToCompare;
 }
