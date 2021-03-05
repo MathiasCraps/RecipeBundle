02931b4f42edf5dcc2d8b152dc855d4ff806fe6a
Mathias Craps <mathias.craps@gmail.com>
2021-03-05 23:23:47 +0100

    Add documentation to Recipe and Ingredient graphql types

--- back-end/src/graphql/queries/Recipes.ts
+++ back-end/src/graphql/queries/Recipes.ts
@@ -4,9 +4,9 @@ import { GraphQLFloat, GraphQLInt, GraphQLString } from 'graphql/type/scalars';
 export const IngredientType = new GraphQLObjectType({
     name: 'ingredient',
     fields: () => ({
-        name: { type: GraphQLString },
-        quantity_number: { type: GraphQLFloat },
-        quantity_description: { type: GraphQLString }
+        name: { type: GraphQLString, description: 'The readable name of the ingredient.' },
+        quantity_number: { type: GraphQLFloat, description: 'The quantitative number of the needed amount.' },
+        quantity_description: { type: GraphQLString, description: 'The readable name of the quantity' }
     })
 });
 
@@ -14,10 +14,10 @@ export const IngredientType = new GraphQLObjectType({
 export const RecipeType = new GraphQLObjectType({
     name: 'recipe',
     fields: () => ({
-        title: { type: GraphQLString },
-        ingredients: { type: new GraphQLList(IngredientType) },
-        steps: { type: GraphQLString },
-        image: { type: GraphQLString },
-        id: { type: GraphQLInt }
+        title: { type: GraphQLString, description: 'The full title of the recipe.' },
+        ingredients: { type: new GraphQLList(IngredientType), description: 'The ingredients needed for the recipe.' },
+        steps: { type: GraphQLString, description: 'The instructions to transform the ingredients to amazing food.' },
+        image: { type: GraphQLString, description: 'An image of the recipe.' },
+        id: { type: GraphQLInt, description: 'A unique identifier of the recipe.' }
     })
 });
