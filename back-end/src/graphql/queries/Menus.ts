a65b3f7573ef7490fd8bc1b3c5b6b45ce2b7c693
Mathias Craps <mathias.craps@gmail.com>
2021-03-05 23:26:02 +0100

    Add description of graphql Menu type

--- back-end/src/graphql/queries/Menus.ts
+++ back-end/src/graphql/queries/Menus.ts
@@ -3,8 +3,8 @@ import { GraphQLFloat, GraphQLInt, GraphQLObjectType } from 'graphql';
 export const MenuType = new GraphQLObjectType({
     name: 'menu',
     fields: () => ({
-        date: { type: GraphQLFloat },
-        menuId: { type: GraphQLInt },
-        recipeId: { type: GraphQLInt }
+        date: { type: GraphQLFloat, description: 'The date of the planned menu. Milliseconds since Unix Epoch.' },
+        menuId: { type: GraphQLInt, description: 'The unique identifier of the menu.' },
+        recipeId: { type: GraphQLInt, description: 'The unique identifier of the recipe.' }
     })
 });
