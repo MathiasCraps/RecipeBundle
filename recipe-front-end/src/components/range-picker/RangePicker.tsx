95bb51d6f1fdaa2d224f260cbbdba98b684289b2
Mathias Craps <mathias.craps@gmail.com>
2021-03-07 23:58:35 +0100

    Rewoork RangePicker to provide selected and picked days (separations of concerns)

--- recipe-front-end/src/components/range-picker/RangePicker.tsx
+++ recipe-front-end/src/components/range-picker/RangePicker.tsx
@@ -1,79 +1,36 @@
-import React, { KeyboardEvent, useState } from 'react';
-import { connect } from 'react-redux';
-import { updateShoppingRange } from '../../redux/Actions';
-import { DateRange, ReduxModel } from '../../redux/Store';
-import { addDays, calculateStartOfDate, calculateStartOfMonthWithOffset } from '../../utils/DateUtils';
+import React, { KeyboardEvent, useEffect, useRef, useState } from 'react';
+import { DateRange } from '../../redux/Store';
+import { addDays, calculateStartOfMonthWithOffset } from '../../utils/DateUtils';
 import { CalendarMonth } from './CalendarMonth';
 
 interface OwnProps {
     showNextMonth: boolean;
     isVisible: boolean;
     onClosing(): void;
+    onDayPicked(date: Date): void;
+    onDaySelected(date: Date): void;
+    selectedRange: DateRange;
+    activeDay: Date;
 }
 
-interface ReduxProps {
-    startDate: Date;
-    endDate: Date;
-}
-
-interface ReduxActions {
-    updateShoppingRange: typeof updateShoppingRange;
-}
-
-function mapStateToProps(reduxModel: ReduxModel): ReduxProps {
-    return {
-        startDate: reduxModel.shoppingDateRange.start,
-        endDate: reduxModel.shoppingDateRange.end
-    }
-}
-
-type Props = OwnProps & ReduxProps & ReduxActions;
-
-enum StageOption {
-    START,
-    END
-}
+type Props = OwnProps;
 
 export const DatePickerContext = React.createContext<DateRange | undefined>(undefined);
-function RangePicker(props: Props) {
+export default function RangePicker(props: Props) {
     if (!props.isVisible) {
         return <div></div>
     }
 
-    const [selection, setSelection] = useState<DateRange | undefined>();
-    const [stage, setStage] = useState<StageOption>(StageOption.START);
+    const [initialOpen, setIntialOpen] = useState(false);
+    const ref = useRef<HTMLDivElement>(null);
 
-    function updateClickedRange(date: Date) {
-        if (isNaN(+date)) {
-            return;
-        }
-
-        if (!selection) {
-            setSelection({ start: date, end: date });
-            setStage(StageOption.END);
-        } else {
-            const [start, end] = [selection.start, date].sort((a, b) => Number(a) - Number(b));
-            setSelection(undefined);
-            setStage(StageOption.START);
-            props.updateShoppingRange({ start, end });
-            props.onClosing();
-        }
-    }
-
-    function toggleView() {
-        if (stage === StageOption.START) {
-            setStage(StageOption.START);
-        } else if (StageOption.END) {
-            setStage(StageOption.START);
-            props.onClosing();
-        }
-    }
+    useEffect(() => {
+        ref.current?.focus();
+    });
 
     function handleKeyAction(event: KeyboardEvent) {
         const key = event.key;
         if (event.key === 'Escape') {
-            setSelection(undefined);
-            setStage(StageOption.START);
             props.onClosing();
             return;
         }
@@ -94,38 +51,21 @@ function RangePicker(props: Props) {
                 break;
         }
 
-        let assigned: DateRange;
+        let assigned: Date;
+        
         if (daysToAdd) {
-            if (stage === StageOption.START) {
-                const baseDateStart: Date = (stage === StageOption.START && selection)
-                    ? selection.start
-                    : calculateStartOfDate(props.startDate);
-                const modified = addDays(baseDateStart, daysToAdd);
-                assigned = { start: modified, end: modified };
-            } else {
-                const baseDateEnd = selection!.end;
-                const modified = addDays(baseDateEnd, daysToAdd);
-                assigned = { start: selection!.start, end: modified };
-            }
-
-            if (assigned) {
-                setSelection(assigned);
-            }
+            assigned = addDays(props.activeDay, daysToAdd)
+            props.onDaySelected(assigned);
         }
 
 
 
-        if (key === 'Enter') {
-            if (selection && stage === StageOption.START) {
-                setStage(StageOption.END);
-            }
+        if (key === 'Enter' && initialOpen) {
+            props.onDayPicked(props.activeDay);
+        }
 
-            if (stage === StageOption.END) {
-                setStage(StageOption.START);
-                setSelection(undefined);
-                props.onClosing();
-                props.updateShoppingRange(assigned! || selection);
-            }
+        if (!initialOpen) {
+            setIntialOpen(true);
         }
     }
 
@@ -137,17 +77,14 @@ function RangePicker(props: Props) {
         months.push(calculateStartOfMonthWithOffset(new Date(), 1));
     }
 
-    return <DatePickerContext.Provider value={selection}>
-        <div onKeyUpCapture={(event) => {
+    return <DatePickerContext.Provider value={props.selectedRange}>
+        <div tabIndex={-1} ref={ref} onKeyUpCapture={(event) => {
             handleKeyAction(event);
-            if (event.key === 'Enter' && stage === StageOption.START) {
-                toggleView()
-            }
         }}>
             {months.map((month, index) => {
                 return <React.Fragment key={index}><CalendarMonth isVisible={props.isVisible}
                     date={month}
-                    onDayPicked={updateClickedRange}
+                    onDayPicked={props.onDayPicked}
                 />
                 </React.Fragment>
 
@@ -155,5 +92,3 @@ function RangePicker(props: Props) {
         </div>
     </DatePickerContext.Provider>
 }
-
-export default connect(mapStateToProps, { updateShoppingRange })(RangePicker);
