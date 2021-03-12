ef47dadab430315ba99aa4eca3f07029966be5fe
Mathias Craps <mathias.craps@gmail.com>
2021-03-12 22:47:06 +0100

    Simplify range color logic by relying on coloring rules

--- recipe-front-end/src/components/range-picker/DayCel.tsx
+++ recipe-front-end/src/components/range-picker/DayCel.tsx
@@ -1,9 +1,8 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { ReduxModel } from '../../redux/Store';
 import { dateIsInRange } from '../../utils/DateUtils';
 import { FillDayFilter } from './dayfilters/FillDayFilter';
-import { DatePickerContext } from './RangePicker';
 
 interface OwnProps {
     day: Date;
@@ -30,18 +29,12 @@ export function mapStateToProps(reduxStore: ReduxModel, ownProps: OwnProps): Red
 type Props = OwnProps & ReduxProps;
 
 function DayCel(props: Props) {
-    const draftSelectedDay = useContext(DatePickerContext);
-    const isInDraftRange = draftSelectedDay
-        ? dateIsInRange(props.day, draftSelectedDay.start, draftSelectedDay.end)
-        : false;
+    const shouldBeColored = props.fillDayFilters.some((rule) => rule(props.day));
     const dayOfMonth = props.day.getDate();
     const valueToRender = !isNaN(dayOfMonth) ? dayOfMonth : '-';
-    const classList = [
-        'picker-day',
-        isInDraftRange || (!draftSelectedDay && props.isInRange) ? 'in-range' : ''
-    ].join(' ');
 
-    return <span tabIndex={-1} className={classList}
+    return <span tabIndex={-1}
+        className={`picker-day ${shouldBeColored ? 'in-range' : ''}`}
         onClick={() => !isNaN(Number(props.day)) && props.onDayPicked(props.day)}
         onMouseOver={() => !isNaN(Number(props.day)) && props.onDaySelected(props.day)}>
         {valueToRender}
