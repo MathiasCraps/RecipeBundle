13801157e67eb6d0ef4da6ac2f2d1837696c5c09
Mathias Craps <mathias.craps@gmail.com>
2021-03-13 20:13:49 +0100

    Show border when day is in selected range

--- recipe-front-end/src/components/range-picker/DayCel.tsx
+++ recipe-front-end/src/components/range-picker/DayCel.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { connect } from 'react-redux';
 import { ReduxModel } from '../../redux/Store';
 import { dateIsInRange } from '../../utils/DateUtils';
 import { FillDayFilter } from './dayfilters/FillDayFilter';
+import { DatePickerContext } from './RangePicker';
 
 interface OwnProps {
     day: Date;
@@ -28,12 +29,15 @@ export function mapStateToProps(reduxStore: ReduxModel, ownProps: OwnProps): Red
 type Props = OwnProps & ReduxProps;
 
 function DayCel(props: Props) {
+    const draftSelectedDay = useContext(DatePickerContext);
     const shouldBeColored = props.fillDayFilters.some((rule) => rule(props.day));
     const dayOfMonth = props.day.getDate();
     const valueToRender = !isNaN(dayOfMonth) ? dayOfMonth : '-';
 
+    const isSelected = draftSelectedDay ? dateIsInRange(props.day, draftSelectedDay.start, draftSelectedDay.end) : false;
+
     return <span tabIndex={-1}
-        className={`picker-day ${shouldBeColored ? 'in-range' : ''}`}
+        className={`picker-day ${shouldBeColored ? 'in-range' : ''} ${isSelected ? 'is-selected' : ''}`}
         onClick={() => !isNaN(Number(props.day)) && props.onDayPicked(props.day)}
         onMouseOver={() => !isNaN(Number(props.day)) && props.onDaySelected(props.day)}>
         {valueToRender}
