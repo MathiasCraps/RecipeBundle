39cae4899d22f5ee44799624eee61914dcba1e4c
Mathias Craps <mathias.craps@gmail.com>
2021-03-13 20:47:32 +0100

    Add default cases

--- recipe-front-end/src/components/range-picker/RangePicker.tsx
+++ recipe-front-end/src/components/range-picker/RangePicker.tsx
@@ -45,6 +45,8 @@ export default function RangePicker(props: Props) {
             case 'ArrowDown':
                 daysToAdd = 7;
                 break;
+            default:
+                // ignore
         }
 
         let assigned: Date;
