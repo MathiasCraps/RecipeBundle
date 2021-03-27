bdd55c09380e602e519dc99093c731f87e5529ab
Mathias Craps <mathias.craps@gmail.com>
2021-03-27 21:52:27 +0100

    Cache categories in memory to improve performance
    Categories are not expected to quickly change

--- back-end/src/sql/GetIngredientCategories.ts
+++ back-end/src/sql/GetIngredientCategories.ts
@@ -2,17 +2,21 @@ import { Pool } from 'pg';
 import { Category } from '../model/RecipeData';
 import { executeQuery } from '../sql-utils/Database';
 
+let cachedCategories: Category[] = [];
 export async function getIngredientCategories(pool: Pool): Promise<Category[]> {
-    const categories = (await executeQuery(pool, {
-        name: 'get-all-ingredients',
-        text: 'SELECT * from IngredientCategory',
-        values: []
-    })).rows;
-
-    return categories.map((category) => {
-        return {
-            categoryId: category.category_id,
-            categoryName: category.category_name
-        }
-    })    
+    if (!cachedCategories.length) {
+        const categories = (await executeQuery(pool, {
+            name: 'get-all-ingredients',
+            text: 'SELECT * from IngredientCategory',
+            values: []
+        })).rows;
+    
+        cachedCategories = categories.map((category) => {
+            return {
+                categoryId: category.id,
+                categoryName: category.category_name
+            }
+        })
+    }
+    return cachedCategories;
 }
