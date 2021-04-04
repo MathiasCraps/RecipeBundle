5520cf5b84126f25d5d66f062a684f51d71b4043
Mathias Craps <mathias.craps@gmail.com>
2021-04-04 22:13:00 +0200

    Use utc for normalization to start of day

--- recipe-front-end/src/utils/DateUtils.ts
+++ recipe-front-end/src/utils/DateUtils.ts
@@ -3,7 +3,7 @@ import { DateRange } from '../redux/Store';
 export const FULL_DAY_IN_MS = 24 * 60 * 60 * 1e3;
 
 export function calculateStartOfDate(date: Date) {
-    return new Date(date.getFullYear(), date.getMonth(), date.getDate())
+    return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate())
 }
 
 export function calculateStartOfMonthWithOffset(date: Date, offset: number) {
