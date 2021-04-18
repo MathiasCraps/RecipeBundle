724119dc22e346d81cda35e1ab72eee0aab20e34
Mathias Craps <mathias.craps@gmail.com>
2021-04-18 17:34:30 +0200

    Expose basic editRecipe functionality on back-end
    Not yet supported: deleting an ingredient + updating the image

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -12,6 +12,7 @@ import { getSessionData } from "./routes/GetSessionData";
 import { addRecipe } from "./sql/AddRecipe";
 import { createCategories } from './sql/CreateCategories';
 import { createTables } from "./sql/CreateTables";
+import { editRecipe } from './sql/EditRecipe';
 import { isRecipe } from "./validation/TypeGuards";
 const multer = require('multer');
 const bodyParser = require('body-parser');
@@ -95,6 +96,37 @@ app.post('/addRecipe', [verifyLoggedIn, async (request: Request, response: Respo
             response.json({error: 'Could not write to database'});
         }
     });  
+}]);
+
+app.post('/editRecipe', [verifyLoggedIn, async (request: Request, response: Response) => {
+    const user = (request.session as SessionData).userName;
+    if (!user) {
+        response.json({error: 'Not logged in'});
+        return;
+    }
+
+    let recipe: Recipe;
+    try {
+        recipe = JSON.parse(request.body.recipe);
+        if (!isRecipe(recipe)) {
+            throw new Error('Invalid');
+        }    
+    } catch (err) {
+        console.log(err);
+        response.json({error: 'Invalid recipe data'});
+        return;
+    }
+
+    // todo, second phase: handle image
+
+    try {
+        await editRecipe(pool, recipe);
+        response.json({success: true });    
+    } catch (err) {
+        // todo for later: remove added image should writing to the database not work
+        console.log('err', err);
+        response.json({error: 'Could not write to database'});
+    }
 }]);
 
 app.get('/getSessionData', async (request, response) => {
