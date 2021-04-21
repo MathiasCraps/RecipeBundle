ec2d814ceabc30889dfed1eae01615cf9662798a
Mathias Craps <mathias.craps@gmail.com>
2021-04-21 22:08:41 +0200

    Add query to read all ingredients from the database

---
+++ back-end/src/sql/GetIngredients.ts
@@ -0,0 +1,23 @@
+import { Pool } from 'pg';
+import { QuantityLessIngredient } from '../model/RecipeData';
+import { executeQuery } from '../sql-utils/Database';
+
+export async function getAllIngredients(pool: Pool): Promise<QuantityLessIngredient[]> {
+    const results = await executeQuery(pool, {
+        name: 'get-all-ingredients',
+        text: `SELECT Ingredients.id as recipe_id, ingredient_name, IngredientCategory.id as category_id, IngredientCategory.category_name
+        FROM Ingredients 
+        INNER JOIN IngredientCategory
+        ON Ingredients.ingredient_category_id = IngredientCategory.id`,
+        values: []
+    });
+
+    return results.rows.map((entry) => {
+        return {
+            id: entry.recipe_id,
+            name: entry.ingredient_name,
+            categoryId: entry.category_id,
+            categoryName: entry.category_name
+        };
+    });
+}
