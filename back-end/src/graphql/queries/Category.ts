18a750fe8eaf0bb68f909bb0ec6041cd994a86ce
Mathias Craps <mathias.craps@gmail.com>
2021-05-03 00:37:33 +0200

    Add Dutch localisation to graphQL-data

--- back-end/src/graphql/queries/Category.ts
+++ back-end/src/graphql/queries/Category.ts
@@ -1,10 +1,19 @@
 import { GraphQLInt, GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql';
 
+export const CategoryLocalisation = new GraphQLObjectType({
+    name: 'categoryLocalisation',
+    description: 'The categories of the ingredients.',
+    fields: () => ({
+        nl: { type: new GraphQLNonNull(GraphQLString), description: 'Dutch translation of ingredient name.' }
+    })
+});
+
 export const Category = new GraphQLObjectType({
     name: 'category',
     description: 'The categories of the ingredients.',
     fields: () => ({
         categoryId: { type: new GraphQLNonNull(GraphQLInt), description: 'The unique identifier of the category.' },
-        categoryName: { type: new GraphQLNonNull(GraphQLString), description: 'The category name'}
+        categoryName: { type: new GraphQLNonNull(GraphQLString), description: 'The category name.' },
+        translations: { type: new GraphQLNonNull(CategoryLocalisation), description: 'Localised category names.' }
     })
 });
