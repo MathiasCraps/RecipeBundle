cd9a71aa0054143b2ee30c28ac2e6d297a9d79d3
Mathias Craps <mathias.craps@gmail.com>
2021-05-03 00:13:18 +0200

    Adjust getIngredientCategories to return updated Category-object

--- back-end/src/sql/GetIngredientCategories.ts
+++ back-end/src/sql/GetIngredientCategories.ts
@@ -1,20 +1,39 @@
 import { Pool } from 'pg';
-import { Category } from '../model/RecipeData';
+import { Category, LocalizedCategory } from '../model/RecipeData';
 import { executeQuery } from '../sql-utils/Database';
 
 let cachedCategories: Category[] = [];
+interface LocalisationLookupMap {
+    [key: string]: LocalizedCategory;
+}
+
 export async function getIngredientCategories(pool: Pool): Promise<Category[]> {
-    if (!cachedCategories.length) {
+    if (!cachedCategories.length || true) {
         const categories = (await executeQuery(pool, {
             name: 'get-all-ingredients',
             text: 'SELECT * from IngredientCategory',
             values: []
         })).rows;
+
+        const categoriesLocalisation: LocalisationLookupMap = (await executeQuery(pool, {
+            name: 'get-all-ingredient-translations',
+            text: 'SELECT * from IngredientCategoryTranslation'
+        })).rows.reduce((previous: LocalisationLookupMap, next) => {
+            const categoryId = String(next.category_id);
+            const languageCode = next.language_code;
+            if (!previous[categoryId]) {
+                previous[categoryId] = {};
+            }
+
+            previous[categoryId][languageCode] = next.localised_name;
+            return previous;
+        }, {});
     
         cachedCategories = categories.map((category) => {
             return {
                 categoryId: category.id,
-                categoryName: category.category_name
+                categoryName: category.category_name,
+                translations: categoriesLocalisation[category.id]
             }
         })
     }
