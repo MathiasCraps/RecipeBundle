b78dac79511bf5d87aff9eff8b9dbeed8f779a7f
Mathias Craps <mathias.craps@gmail.com>
2021-05-13 23:57:45 +0200

    Add ModifyStorageResponse

---
+++ back-end/src/graphql/mutation/ModifyStorageResponse.ts
@@ -0,0 +1,10 @@
+import { GraphQLBoolean, GraphQLString, GraphQLObjectType, GraphQLNonNull } from 'graphql';
+
+export const ModifyStorage = new GraphQLObjectType({
+    name: 'modifyStorage',
+    description: 'Server reply after modifying a recipe.',
+    fields: () => ({
+        success: { type: GraphQLNonNull(GraphQLBoolean), description: 'Indicates if the operation was executed successfully' },
+        error: { type: GraphQLString, description: 'Describes why the operation has not finished' }
+    })
+});
