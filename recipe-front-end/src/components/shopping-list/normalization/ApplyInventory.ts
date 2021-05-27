25e716adf30474598ccd748f55e0e789b38121d0
Mathias Craps <mathias.craps@gmail.com>
2021-05-27 23:22:30 +0200

    Implement logic to show missing ingredients if they are not part of a menu
    Needs to be cleaned up in follow-up commits

--- recipe-front-end/src/components/shopping-list/normalization/ApplyInventory.ts
+++ recipe-front-end/src/components/shopping-list/normalization/ApplyInventory.ts
@@ -2,9 +2,11 @@ import { Ingredient } from '../../../interfaces/Recipe';
 import { InventoryItem } from '../../../redux/Store';
 import { LinkedMap } from '../../../utils/ArrayUtils';
 
-export function applyInventory(ingredients: Ingredient[], inventoryMap: LinkedMap<InventoryItem>): Ingredient[] {
-    return ingredients.map((ingredient) => {
+export function applyInventory(ingredients: Ingredient[], inventoryMap: LinkedMap<InventoryItem>): Ingredient[] {   
+    const normalResults =  ingredients.map((ingredient) => {
         const entry = inventoryMap[ingredient.id];
+        delete inventoryMap[ingredient.id];
+
         if (!entry) {
             return ingredient;
         }
@@ -15,4 +17,26 @@ export function applyInventory(ingredients: Ingredient[], inventoryMap: LinkedMa
             quantity_number: ingredient.quantity_number + difference
         };
     });
+
+    const keys = Object.keys(inventoryMap);
+    const extraResults: Ingredient[] = [];
+    for (const key of keys) {
+        const { ingredient, desiredQuantity, quantity } = inventoryMap[key];
+        const difference = desiredQuantity - quantity;
+
+        if (difference > 0) {
+            extraResults.push({
+                ...ingredient,
+                quantity_number: difference,
+                quantity_description: '', // todo: link
+                category: {
+                    categoryId: 99999,
+                    categoryName: 'Varia',
+                    translations: {nl: 'Divers'}
+                }
+            })
+        }
+    }
+
+    return normalResults.concat(extraResults);
 }
