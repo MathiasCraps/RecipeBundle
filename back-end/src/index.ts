a91fccee09796ac925f32db645135f49fd2da710
Mathias Craps <mathias.craps@gmail.com>
2021-06-01 23:56:34 +0200

    Populate database with default quantitydescriptions for initial set-up

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -16,6 +16,7 @@ import { createCategories } from './sql/ingredient/CreateCategories';
 import { createTables } from "./sql/create-table/CreateTables";
 import { editRecipe } from './sql/recipe/EditRecipe';
 import { isRecipe } from "./validation/TypeGuards";
+import { createQuantityDescriptions } from './sql/ingredient/CreateQuantityDescription';
 const multer = require('multer');
 const bodyParser = require('body-parser');
 
@@ -205,6 +206,7 @@ pool.connect(async (error, client, done) => {
         if (initialSetUp) {
             const startData: StartData = JSON.parse(fs.readFileSync(`${__dirname}/categories.json`, 'utf8'));
             createCategories(pool, startData.categories);
+            createQuantityDescriptions(pool, startData.quantityDescriptions);
         }
     } catch (err) {
         console.log('error setting up tables', err);
