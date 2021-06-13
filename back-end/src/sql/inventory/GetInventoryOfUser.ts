1a242d2f03d55932160c80ab8a9cba4ae0dbf2aa
Mathias Craps <mathias.craps@gmail.com>
2021-06-13 22:09:52 +0200

    Fix incorrect join query - caused issues with outputing inventory records

--- back-end/src/sql/inventory/GetInventoryOfUser.ts
+++ back-end/src/sql/inventory/GetInventoryOfUser.ts
@@ -2,14 +2,17 @@ import { Pool } from 'pg';
 import { InventoryItem } from '../../model/RecipeData';
 import { executeQuery } from '../../sql-utils/Database';
 
-export async function getInventoryOfUser(pool: Pool, userId: number): Promise<InventoryItem[]> {  
+export async function getInventoryOfUser(pool: Pool, userId: number): Promise<InventoryItem[]> {      
     const results = (await executeQuery(pool, {
         name: 'get-inventory',
-        text: `SELECT ingredient_id, quantity, desired_quantity, Ingredients.ingredient_quantity_id as ingredient_quantity_id FROM Inventory 
+        text: `SELECT ingredient_id, 
+            quantity, 
+            desired_quantity, 
+            ingredient_quantity_id as ingredient_quantity_id 
+        FROM Inventory 
         INNER JOIN Ingredients
-        ON Inventory.ingredient_id = Ingredients.ingredient_quantity_id
-        WHERE user_id = $1`,
-        values: [userId]
+        ON Inventory.ingredient_id = Ingredients.id`,
+        values: []
     })).rows;
 
     return results.map((result) => {
