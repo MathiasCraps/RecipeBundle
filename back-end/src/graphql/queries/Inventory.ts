563fe5a1eb5f400442c79b3d54c3c287df2f07a0
Mathias Craps <mathias.craps@gmail.com>
2021-06-15 22:09:36 +0200

    Fix issue where quantity would be exposed as as string-number instead of a number

--- back-end/src/graphql/queries/Inventory.ts
+++ back-end/src/graphql/queries/Inventory.ts
@@ -1,11 +1,11 @@
-import { GraphQLFloat, GraphQLInt, GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql';
+import { GraphQLFloat, GraphQLInt, GraphQLNonNull, GraphQLObjectType } from 'graphql';
 
 export const InventoryItem = new GraphQLObjectType({
     name: 'Inventory',
     description: 'A storage inventory item',
     fields: () => ({
         ingredientId: { type: new GraphQLNonNull(GraphQLInt), description: 'The ingredient id that is in the storage.' },
-        quantity: { type: new GraphQLNonNull(GraphQLString), description: 'The quantity of the storage item.' },
+        quantity: { type: new GraphQLNonNull(GraphQLFloat), description: 'The quantity of the storage item.' },
         desiredQuantity: { type: GraphQLInt, description: 'The desired reserve stock the user wishes to keep after all menus have been made.'},
         quantity_description_id: { type: GraphQLFloat, description: 'The identifier of a description quantity.'}
     })
