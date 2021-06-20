ee4bbd3aae578bd705c4f287b128197f0af0ceaf
Mathias Craps <mathias.craps@gmail.com>
2021-06-20 23:46:36 +0200

    Add basic UI components for creating an ingredient via inventory modal
    Interactive logic to be added in follow-up

--- recipe-front-end/src/components/inventory/InventoryModal.tsx
+++ recipe-front-end/src/components/inventory/InventoryModal.tsx
@@ -1,9 +1,10 @@
 import { Modal } from '@chakra-ui/modal';
-import { Button, Input, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useToast } from '@chakra-ui/react';
+import { Button, Input, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Select, useToast } from '@chakra-ui/react';
 import React, { useRef, useState } from 'react';
+import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
-import { BaseIngredient } from '../../interfaces/Recipe';
+import { BaseIngredient, Category, QuantityDescription } from '../../interfaces/Recipe';
 import { Localisation } from '../../localisation/AppTexts';
 import { updateInventoryAction, updateInventoryActionReturn } from '../../redux/Actions';
 import { InventoryItem, ReduxModel, UpdateInventoryAction, UpdateInventoryModification } from '../../redux/Store';
@@ -18,6 +19,8 @@ interface OwnProps {
 
 interface ReduxProps {
     ingredients: BaseIngredient[];
+    quantityDescriptions: QuantityDescription[];
+    categories: Category[];
 }
 
 interface ReduxActions {
@@ -45,6 +48,7 @@ function InventoryModal(props: Props) {
     const [quantity, setQuantity] = useState<number | undefined>(props.initialValue?.quantity);
     const [desiredQuantity, setDesiredQuantity] = useState<number | undefined>(props.initialValue?.desiredQuantity);
     const toast = useToast();
+    const [creatingNewIngredient, setCreatingNewIngredient] = useState(true); // todo, make dynamic in follow-up
     const canBeSubmitted = Boolean(selection && (typeof quantity === 'number' && quantity >= 0) || (typeof desiredQuantity === 'number' && desiredQuantity >= 0));
 
     return <Modal isOpen={props.isOpened} onClose={props.onCancel} initialFocusRef={props.initialValue ? fallbackRef : ref}>
@@ -65,26 +69,72 @@ function InventoryModal(props: Props) {
                     label={Localisation.INGREDIENT}
                 />
 
-                <div>
-                    <label>{Localisation.QUANTITY}<br />
-                        <Input
-                            type="number"
-                            placeholder={Localisation.QUANTITY}
-                            value={typeof quantity === 'number' ? quantity : ''}
-                            ref={fallbackRef}
-                            onChange={(e) => !Number.isNaN(e.target.value) && setQuantity(Number(e.target.value))}
-                        />
+                {creatingNewIngredient && <div>
+                    <p>{Localisation.INGREDIENT_DOES_NOT_EXIST_ADDING}</p>
+                    <label>
+                        {Localisation.QUANTITY_KIND}
+                        <Select disabled={!creatingNewIngredient} value={selection?.quantityDescription?.quantityDescriptorId} onChange={(e) => {
+                            const match = props.quantityDescriptions.find((entry) => {
+                                return String(entry.quantityDescriptorId) === e.target.selectedOptions[0].value
+                            });
+                            if (match && selection) {
+                                setSelection({
+                                    ...selection,
+                                    quantityDescription: match,
+                                    quantity_description_id: match.quantityDescriptorId
+                                })
+                            }
+                        }}>
+                            {props.quantityDescriptions.map((description, index) => {
+                                const text = description.translations['nl'];
+
+                                const capitalizedText = text.charAt(0).toUpperCase() + text.substr(1);
+                                return <option key={index} value={description.quantityDescriptorId}>{capitalizedText}</option>
+                            })}
+                        </Select>
                     </label>
 
-                    <label>{Localisation.DESIRED_QUANTITY}<br />
-                        <Input
-                            type="number"
-                            placeholder={Localisation.DESIRED_QUANTITY}
-                            value={typeof desiredQuantity === 'number' ? desiredQuantity : ''}
-                            onChange={(e) => !Number.isNaN(e.target.value) && setDesiredQuantity(Number(e.target.value))}
-                        />
+                    <label>
+                        {Localisation.CATEGORY_INGREDIENT}
+                        <Select disabled={!creatingNewIngredient} value={selection?.categoryId} onChange={(e) => {
+                            if (selection) {
+                                setSelection({
+                                    ...selection,
+                                    categoryId: Number(e.target.selectedOptions[0].value)
+                                });
+                            }
+
+                        }}>
+                            {props.categories.map((category) => {
+                                return <React.Fragment key={category.categoryId}>
+                                    <option value={category.categoryId}>
+                                        {category.translations.nl}
+                                    </option>
+                                </React.Fragment>
+                            })}
+                        </Select>
                     </label>
-                </div>
+                </div>}
+
+
+                <label>{Localisation.QUANTITY}<br />
+                    <Input
+                        type="number"
+                        placeholder={Localisation.QUANTITY}
+                        value={typeof quantity === 'number' ? quantity : ''}
+                        ref={fallbackRef}
+                        onChange={(e) => !Number.isNaN(e.target.value) && setQuantity(Number(e.target.value))}
+                    />
+                </label>
+
+                <label>{Localisation.DESIRED_QUANTITY}<br />
+                    <Input
+                        type="number"
+                        placeholder={Localisation.DESIRED_QUANTITY}
+                        value={typeof desiredQuantity === 'number' ? desiredQuantity : ''}
+                        onChange={(e) => !Number.isNaN(e.target.value) && setDesiredQuantity(Number(e.target.value))}
+                    />
+                </label>
             </ModalBody>
 
             <ModalFooter>
