dd60b20cb18c98a1f2654b7a3cdd6a4928cd5feb
Mathias Craps <mathias.craps@gmail.com>
2021-06-23 23:48:20 +0200

    Add ingredientId to graphql model

--- back-end/src/graphql/mutation/ModifyStorageResponse.ts
+++ back-end/src/graphql/mutation/ModifyStorageResponse.ts
@@ -1,10 +1,11 @@
-import { GraphQLBoolean, GraphQLString, GraphQLObjectType, GraphQLNonNull } from 'graphql';
+import { GraphQLBoolean, GraphQLString, GraphQLObjectType, GraphQLNonNull, GraphQLFloat } from 'graphql';
 
 export const ModifyStorage = new GraphQLObjectType({
     name: 'modifyStorage',
     description: 'Server reply after modifying a recipe.',
     fields: () => ({
         success: { type: GraphQLNonNull(GraphQLBoolean), description: 'Indicates if the operation was executed successfully' },
-        error: { type: GraphQLString, description: 'Describes why the operation has not finished' }
+        error: { type: GraphQLString, description: 'Describes why the operation has not finished' },
+        ingredientId: { type: GraphQLFloat, description: 'The identifier of the new ingredient. Empty when creation failed.'}
     })
 });
