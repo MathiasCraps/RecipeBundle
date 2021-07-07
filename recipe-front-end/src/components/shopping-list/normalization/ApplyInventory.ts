8ed0134a8da33fbd70ff7445378af0892285521e
Mathias Craps <mathias.craps@gmail.com>
2021-07-07 23:24:56 +0200

    Simplify logic further by moving repeated logic to shared function
    Rename function to make new expectation clearer

--- recipe-front-end/src/components/shopping-list/normalization/ApplyInventory.ts
+++ recipe-front-end/src/components/shopping-list/normalization/ApplyInventory.ts
@@ -11,46 +11,40 @@ const baseQuantityAndCategory = {// to be replaced in follow-up with linked data
     }
 };
 
-function calculateIngredientDifference(
+function applyInventoryAndPushUsableEntries(
     baseIngredient: BaseIngredient,
     rawRequired: number,
-    { quantity: availableQuantity, desiredQuantity }: InventoryItem
-): QuantifiedIngredient | undefined {
+    { quantity: availableQuantity, desiredQuantity }: InventoryItem,
+    existingEntries: QuantifiedIngredient[]
+): QuantifiedIngredient[] {
     const quantityNumber = rawRequired + (desiredQuantity - availableQuantity);
 
     if (quantityNumber <= 0) {
-        return undefined;
+        return existingEntries;
     }
 
-    return {
+    return existingEntries.concat([{
         ...baseIngredient,
         quantity_number: quantityNumber,
         ...baseQuantityAndCategory
-    }
+    }]);
 }
 
 
 export function applyInventory(ingredients: QuantifiedIngredient[], inventoryMap: LinkedMap<InventoryItem>): QuantifiedIngredient[] {
     const results: QuantifiedIngredient[] = [];
 
+    // ingredients in scheduled recipes
     ingredients.forEach((ingredient: QuantifiedIngredient) => {
         delete inventoryMap[ingredient.id];
-
-        const presentableQuantifiedIngredient = calculateIngredientDifference(ingredient, ingredient.quantity_number, inventoryMap[ingredient.id]);
-        if (presentableQuantifiedIngredient) {
-            results.push(presentableQuantifiedIngredient);
-        }
+        applyInventoryAndPushUsableEntries(ingredient, ingredient.quantity_number, inventoryMap[ingredient.id], results);
     });
 
     // ingredients in inventory, but not in scheduled recipes
     const keys = Object.keys(inventoryMap);
     for (const key of keys) {
         const inventoryItem = inventoryMap[key];
-        const presentableQuantifiedIngredient = calculateIngredientDifference(inventoryItem.ingredient, 0, inventoryItem)
-
-        if (presentableQuantifiedIngredient) {
-            results.push(presentableQuantifiedIngredient)
-        }
+        applyInventoryAndPushUsableEntries(inventoryItem.ingredient, 0, inventoryItem, results);
     }
 
     return results;
